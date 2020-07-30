ef238605cb15daa1b31a6b151d921b037da20b74
Dishy.Dev <dishy@dev>
2020-07-30 20:13:32 +0000

    Sort of working baseline

--- Word-Count.py
+++ Word-Count.py
@@ -4,6 +4,8 @@ import luigi
 from bs4 import BeautifulSoup
 from collections import Counter
 
+NUM_BOOKS = 10
+REPLACE_LIST = """.,"';_[]:*"""
 
 class GetTopBooks(luigi.Task):
     def output(self):
@@ -16,13 +18,17 @@ class GetTopBooks(luigi.Task):
         pageHeader = soup.find_all("h2", string="Top 100 EBooks yesterday")[0]
         listTop = pageHeader.find_next_sibling("ol")
         with self.output().open("w") as f:
+            resultCounter = 0
             for result in listTop.select("li>a"):
                 if "/ebooks/" in result["href"]:
+                    resultCounter += 1
                     f.write(
-                        "http://www.gutenberg.org/browse/scores{link}\n".format(
+                        "http://www.gutenberg.org/{link}.txt.utf-8\n".format(
                             link=result["href"]
                         )
                     )
+                    if resultCounter >= NUM_BOOKS:
+                        break
 
 
 class DownloadBooks(luigi.Task):
@@ -30,27 +36,35 @@ class DownloadBooks(luigi.Task):
         return GetTopBooks()
 
     def output(self):
-
-        return luigi.LocalTarget("data/results.txt")
+        outputTargets = []
+        for i in range(NUM_BOOKS):
+            outputTargets.append( luigi.LocalTarget("data/downloads/{}.txt".format(i)) )
+        return outputTargets
 
     def run(self):
         with self.input().open("r") as i:
             for i, line in enumerate(i.read().splitlines()):
-                resp = requests.get(line)
-        with self.output().open("w") as f:
-            f.write("Hi")
+                with self.output()[i].open("w") as outfile:
+                    resp = requests.get(line)
+                    filtered_text = resp.text
+                    for char in REPLACE_LIST:
+                        filtered_text = filtered_text.replace(char, " ")
+                    outfile.write(filtered_text)
 
 
 class CountWords(luigi.Task):
     fileId = luigi.Parameter()
 
+    def requires(self):
+        return DownloadBooks()
+
     def output(self):
         return luigi.LocalTarget(
-            "data/count_{}.pickle".format(self.fileId), format=luigi.format.Nop
+            "data/counts/count_{}.pickle".format(self.fileId), format=luigi.format.Nop
         )
 
     def run(self):
-        with open("data/{}.txt".format(self.fileId)) as file:
+        with open("data/downloads/{}.txt".format(self.fileId)) as file:
             word_count = Counter(file.read().split())
             with self.output().open("w") as outfile:
                 pickle.dump(word_count, outfile)
@@ -58,7 +72,10 @@ class CountWords(luigi.Task):
 
 class ConCat(luigi.Task):
     def requires(self):
-        return [CountWords(fileId=1), CountWords(fileId=2), CountWords(fileId=3)]
+        requiredInputs = []
+        for i in range(NUM_BOOKS):
+            requiredInputs.append( CountWords(fileId=i) )
+        return requiredInputs
 
     def output(self):
         return luigi.LocalTarget("data/summary.txt")
