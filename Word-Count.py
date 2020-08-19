fc2017a2c0c7001162356684d4850d6b60d3d96b
Dishy.Dev <dishy@dev>
2020-08-19 07:54:06 +0000

    Update to latest

--- Word-Count.py
+++ Word-Count.py
@@ -1,11 +1,8 @@
-import pickle
 import requests
 import luigi
 from bs4 import BeautifulSoup
 from collections import Counter
-
-REPLACE_LIST = """.,"';_[]:*-"""
-
+import pickle
 
 class GetTopBooks(luigi.Task):
     """
@@ -13,81 +10,101 @@ class GetTopBooks(luigi.Task):
     """
 
     def output(self):
-        return luigi.LocalTarget(
-            "data/{}_bookslist.txt".format(GlobalParams().NUMBER_BOOKS)
-        )
+        return luigi.LocalTarget("data/books_list.txt")
 
     def run(self):
         # Download the contents of the top page
         resp = requests.get("http://www.gutenberg.org/browse/scores/top")
+        
         soup = BeautifulSoup(resp.content, "html.parser")
+        
         # Get the header from the page
         pageHeader = soup.find_all("h2", string="Top 100 EBooks yesterday")[0]
-
         listTop = pageHeader.find_next_sibling("ol")
+        
+        # Open the Luigi Target file
         with self.output().open("w") as f:
-            resultCounter = 0
+            # For each item in the top books list, extract the
+            # URL of the book and convert it into a download link
             for result in listTop.select("li>a"):
                 if "/ebooks/" in result["href"]:
-                    resultCounter += 1
-                    f.write(
-                        "http://www.gutenberg.org/{link}.txt.utf-8\n".format(
+                    f.write("http://www.gutenberg.org{link}.txt.utf-8\n"
+                        .format(
                             link=result["href"]
                         )
                     )
-                    print(GlobalParams.NUMBER_BOOKS)
-                    if resultCounter >= GlobalParams().NUMBER_BOOKS:
-                        break
-
-
-class GlobalParams(luigi.Config):
-    NUMBER_BOOKS = luigi.IntParameter(default=10)
-    NUMBER_WORDS = luigi.IntParameter(default=500)
 
 
 class DownloadBooks(luigi.Task):
-    fileId = luigi.Parameter()
+    """
+    Download a specified list of books
+    """
+    FileID = luigi.IntParameter()
+
+    # List of characters to remove from the downloaded books
+    REPLACE_LIST = """.,"';_[]:*-\t"""
 
     def requires(self):
         return GetTopBooks()
 
     def output(self):
-        return luigi.LocalTarget("data/downloads/{}.txt".format(self.fileId))
+        return luigi.LocalTarget("data/downloads/{}.txt".format(self.FileID))
 
     def run(self):
         with self.input().open("r") as i:
-            URL = i.read().splitlines()[self.fileId]
+            # Open the line specified by the FileID parameter
+            URL = i.read().splitlines()[self.FileID]
+            
             with self.output().open("w") as outfile:
-                resp = requests.get(URL)
-                filtered_text = resp.text
-                for char in REPLACE_LIST:
-                    filtered_text = filtered_text.replace(char, " ")
-                outfile.write(filtered_text.lower())
+                # Download the book contents from Project Gutenberg
+                book_downloads = requests.get(URL)
+                book_text = book_downloads.text
+                
+                # Replace any special characters from the book text
+                for char in self.REPLACE_LIST:
+                    book_text = book_text.replace(char, " ")
+                
+                # Convert the contents of the book to lower case so we can
+                # compare works with different cases
+                book_text = book_text.lower()
+                outfile.write(book_text)
 
 
 class CountWords(luigi.Task):
-    fileId = luigi.Parameter()
+    """
+    Count the frequency of the most common words from a file
+    """
+
+    FileID = luigi.IntParameter()
 
     def requires(self):
-        return DownloadBooks(fileId=self.fileId)
+        return DownloadBooks(FileID=self.FileID)
 
     def output(self):
         return luigi.LocalTarget(
-            "data/counts/count_{}.pickle".format(self.fileId), format=luigi.format.Nop
+            "data/counts/count_{}.pickle".format(self.FileID),
+            format=luigi.format.Nop
         )
 
     def run(self):
-        with open("data/downloads/{}.txt".format(self.fileId)) as file:
-            word_count = Counter(file.read().split())
+        with self.input().open("r") as i:
+            word_count = Counter(i.read().split())
             with self.output().open("w") as outfile:
                 pickle.dump(word_count, outfile)
 
+class GlobalParams(luigi.Config):
+    NumberBooks = luigi.IntParameter(default=10)
+    NumberTopWords = luigi.IntParameter(default=500)
 
 class TopWords(luigi.Task):
+    """
+    Aggregate the count results from the different files
+    """
+
     def requires(self):
         requiredInputs = []
-        for i in range(GlobalParams().NUMBER_BOOKS):
-            requiredInputs.append(CountWords(fileId=i))
+        for i in range(GlobalParams().NumberBooks):
+            requiredInputs.append(CountWords(FileID=i))
         return requiredInputs
 
     def output(self):
@@ -101,5 +118,5 @@ class TopWords(luigi.Task):
                 counters += nextCounter
         #
         with self.output().open("w") as f:
-            for item in counters.most_common(GlobalParams().NUMBER_WORDS):
-                f.write("{}\t{}\n".format(*item))
+            for item in counters.most_common(GlobalParams().NumberTopWords):
+                f.write("{0: <15}{1}\n".format(*item))
